6d7146ec29166cc9e35fa5211a2ac9a83bb045d1
185717 <185717@USTEXPLNDYK4QN2.AMERICAS.GLOBAL.NTTDATA.COM>
2019-06-28 13:33:42 -0500

    firstcommit

---
+++ Demo/src/com/Demo/StringDemo.java
@@ -0,0 +1,16 @@
+package com.Demo;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s1="Hello";
+		String s2="World";
+		if(s1.equals(s2))
+			System.out.println("String identical");
+		else
+			System.out.println("Not same");
+	}
+
+}
